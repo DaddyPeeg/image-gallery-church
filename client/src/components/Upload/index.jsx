71f09ee4d180d789630d4814d81f7c64f258d0dc
David <david.estrelloso.tribugenia@gmail.com>
2023-11-06 13:51:08 +0800

    Updated Modal

--- client/src/components/Upload/index.jsx
+++ client/src/components/Upload/index.jsx
@@ -7,6 +7,11 @@ const ModalUpload = ({ modalOpen, setModalOpen }) => {
   function onSetFiles(event) {
     const files = event.target.files;
     const filesArray = Array.from(files);
+    if (filesArray.length > 10) {
+      event.preventDefault();
+      alert("Cannot upload more than 10 files");
+      return;
+    }
 
     const imagesArray = filesArray.map((file) => {
       return URL.createObjectURL(file);
@@ -14,14 +19,31 @@ const ModalUpload = ({ modalOpen, setModalOpen }) => {
 
     setSelectedImages(imagesArray);
   }
-
+  console.log(selectedImages);
   const isImages = () => {
     if (!selectedImages.length > 0)
       return {
         justifyContent: "center",
         alignItems: "center",
+        color: "#545454",
       };
   };
+
+  const UploadImages = () => {
+    if (selectedImages.length > 0) {
+      console.log(selectedImages);
+    }
+  };
+
+  const btnDisabled = (n) => {
+    if (n)
+      return {
+        opacity: "0.3",
+        cursor: "not-allowed",
+      };
+  };
+
+  const logic = selectedImages.length === 0 ? true : false;
 
   if (modalOpen)
     return (
@@ -33,24 +55,50 @@ const ModalUpload = ({ modalOpen, setModalOpen }) => {
           }}
         ></div>
         <div className="modal">
-          <label>
-            + Add Images
-            <br />
-            <span>Up to 10 Images</span>
-            <input
-              type="file"
-              name="imagesUpload"
-              onChange={onSetFiles}
-              multiple
-              accept="image/png, image/jpeg, image/webp"
-            />
-          </label>
+          <div className="modal-control">
+            <label>
+              + Add Images
+              <br />
+              <span>Up to 10 Images</span>
+              <input
+                type="file"
+                name="imagesUpload"
+                onChange={onSetFiles}
+                multiple
+                accept="image/png, image/jpeg, image/webp"
+              />
+            </label>
+            <div className="modal-btn">
+              <button
+                style={btnDisabled(logic)}
+                onClick={() => UploadImages()}
+                disabled={logic}
+              >
+                Upload
+              </button>
+              <button
+                style={btnDisabled(logic)}
+                onClick={() => setSelectedImages([])}
+                disabled={logic}
+              >
+                Clear
+              </button>
+            </div>
+          </div>
           <div className="imageSet" style={isImages()}>
             {selectedImages.length > 0
               ? selectedImages.map((image, index) => {
                   return (
                     <div key={`${image}-${index}`}>
-                      <img src={image} alt={`${image}-${index}`} />
+                      <img
+                        src={image}
+                        alt={`${image}-${index}`}
+                        onClick={() =>
+                          setSelectedImages(
+                            selectedImages.filter((item, i) => i !== index)
+                          )
+                        }
+                      />
                     </div>
                   );
                 })
